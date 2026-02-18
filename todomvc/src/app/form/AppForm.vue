cabc4847a439de328bec6523e7a70ecb235a16b4
Matteo Antony Mistretta <antony.mistretta@gmail.com>
2026-02-18 17:01:30 +0100

    Explain composable

---
+++ todomvc/src/app/form/AppForm.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useList } from '../list/list'
+import { useForm } from './form'
+
+const { text, empty } = useForm()
+const { add } = useList()
+
+function handleSubmit() {
+  add(text.value)
+  empty()
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="What next?" autofocus v-model="text" />
+    <button :disabled="!text">Add</button>
+  </form>
+</template>
---
+++ todomvc/src/app/list/list.js
@@ -0,0 +1,24 @@
+import { ref } from 'vue'
+
+const tasks = ref([
+  { id: 1, text: 'Learn Vue', completed: true },
+  { id: 2, text: 'Look for a job', completed: false },
+  { id: 3, text: 'Forget everything' },
+])
+
+export function useList() {
+  function add(text) {
+    const maxId = tasks.value.length ? tasks.value[tasks.value.length - 1].id : 0
+    tasks.value.push({ id: maxId + 1, text })
+  }
+
+  function toggle(index) {
+    tasks.value[index].completed = !tasks.value[index].completed
+  }
+
+  function remove(index) {
+    tasks.value.splice(index, 1)
+  }
+
+  return { tasks, add, toggle, remove }
+}
