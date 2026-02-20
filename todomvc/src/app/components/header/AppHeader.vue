9b7efbdb8f14e2cdf78d026f8351af15ac1f024a
Matteo Antony Mistretta <antony.mistretta@gmail.com>
2026-02-20 13:00:46 +0100

    Explain Vite optimization (WIP)

--- todomvc/src/app/components/header/AppHeader.vue
+++ todomvc/src/app/components/header/AppHeader.vue
@@ -1,7 +1,12 @@
 <script setup>
+import ic from '@/assets/logo.png?w=64&format=webp'
+import governo from '@/assets/governo.svg'
+
 defineProps(['name'])
 </script>
 
 <template>
   <h1>{{ name }}'s Todo List</h1>
+  <img :src="ic" alt="ic-logo" width="64" />
+  <img :src="governo" alt="governo-logo" width="64" />
 </template>
---
+++ todomvc/vite.optimized.config.js
@@ -0,0 +1,131 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
+import { VitePWA } from 'vite-plugin-pwa'
+import { imagetools } from 'vite-imagetools'
+import inspect from 'vite-plugin-inspect'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+
+    visualizer({
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'dist/stats.html',
+    }),
+
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 10240,
+      deleteOriginFile: false,
+    }),
+
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'My Vue App',
+        short_name: 'VueApp',
+        theme_color: '#fffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/api\.example\.com\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60,
+              },
+            },
+          },
+        ],
+      },
+    }),
+
+    // Uso: import img from './image.jpg?w=400&format=webp'
+    imagetools(),
+
+    inspect(),
+
+    ViteImageOptimizer(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  css: {
+    devSourcemap: true,
+
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`,
+      },
+    },
+  },
+
+  optimizeDeps: {
+    include: ['vue', 'pinia', 'axios'],
+    exclude: ['@vueuse/core'],
+  },
+
+  build: {
+    target: 'esnext',
+
+    minify: 'terser', // 'esbuild'
+
+    reportCompressedSize: false,
+
+    chunkSizeWarningLimit: 1000, // 500kb default
+
+    cssCodeSplit: true,
+
+    cssMinify: 'lightningcss', // 'esbuild'
+
+    sourcemap: false,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'pinia', 'axios'],
+        },
+
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+  },
+})
